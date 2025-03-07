e518e37004eb01b24f7406261455858d3f9e120d
CreativeEngine28 <157041089+CreativeEngine28@users.noreply.github.com>
2025-03-07 14:54:12 +0000

    fixed I think

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -16,7 +16,7 @@ function runProgram(){
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
