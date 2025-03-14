99282d4a343a709508b8e03de6ce271d507b1aaf
CreativeEngine28 <157041089+CreativeEngine28@users.noreply.github.com>
2025-03-14 13:20:07 +0000

    added walker var

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -18,7 +18,13 @@ function runProgram(){
     DOWN: 40,
   }
   // Game Item Objects
+var walker = {
+  x: 0,
+  y: 0,
+  speedX: 0,
+  speedY: 0,
 
+}
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
